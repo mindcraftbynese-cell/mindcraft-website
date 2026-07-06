806aa2b99091d1a571e97c03d89f4931c4da6d8e
mindcraftbynese-cell <mindcraftbynese@gmail.com>
2026-07-06 09:10:57 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      { source: '/hizmetlerimiz', destination: '/hizmetler', permanent: true },
+      { source: '/gorsel-galeri', destination: '/galeri', permanent: true },
+      { source: '/bizden-haberler', destination: '/', permanent: true },
+      { source: '/f/jill-bolte-taylor-tutkun-tutkunu', destination: '/referanslar', permanent: true },
+      { source: '/f/noro-liderlikte-karar-alma-ve-problem-cozme-surecleri', destination: '/referanslar', permanent: true },
+      { source: '/f/noro-liderlik-nedir-sizi-neden-daha-iyi-bir-lider-yapar', destination: '/referanslar', permanent: true },
+    ];
+  },
 };
 
-export default nextConfig;
+export default
